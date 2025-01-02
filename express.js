56dd56d7dbaa00ac8b0e23aba99064c8ae325cb5
Matija Marohnić <matija.marohnic@gmail.com>
2025-01-02 22:38:12 +0100

    Add extensions behind an environment variable

--- express.js
+++ express.js
@@ -1,5 +1,9 @@
 import express from 'express'
 
 const app = express()
-app.use(express.static('website', { extensions: ['html'] }))
+app.use(express.static('website', {
+  extensions: process.env.HTML_EXTENSIONS === 'true'
+    ? ['htm', 'html']
+    : false
+}))
 app.listen(3001)
