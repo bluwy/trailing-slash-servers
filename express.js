84b5ac2277508eba19f20522ce52b38effac6c62
Matija Marohnić <matija.marohnic@gmail.com>
2025-01-07 09:28:35 +0100

    Add Express with default .html extensions (#2)

--- express.js
+++ express.js
@@ -1,5 +1,9 @@
 import express from 'express'
 
 const app = express()
-app.use(express.static('website'))
+app.use(express.static('website', {
+  extensions: process.env.HTML_EXTENSIONS === 'true'
+    ? ['htm', 'html']
+    : false
+}))
 app.listen(3001)
